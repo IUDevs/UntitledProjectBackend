264d408a6064ab069483ce48aa827750ba8ccf5a
Emmanuel <emmopk11@outlook.com>
2024-03-21 22:44:58 +0100

    trying a main branch push

--- src/routes/getAll.ts
+++ src/routes/getAll.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  return res.send("All flashcards");
+  return res.send("All flashcard");
 });
 
 export default router;
